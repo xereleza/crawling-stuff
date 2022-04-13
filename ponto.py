6ba9d8a2638052eff6f83f7fa7059b76e9897f37
xereleza <pretzeldaniel@gmail.com>
2022-04-13 08:38:42 -0300

    fix: action inside .do() needed to be callable

--- ponto.py
+++ ponto.py
@@ -3,16 +3,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
-#chrome_options.add_argument("--disable-extensions")
-#chrome_options.add_argument("--disable-gpu")
-#chrome_options.add_argument("--no-sandbox") # linux only
+chrome_options.add_argument("--no-sandbox") # linux only
 chrome_options.add_argument("--headless")
-# chrome_options.headless = True # also works
 
-from time import sleep
 import schedule
 
-def bater_ponto(user,password):
+def crawl(user,password):
     driver=Chrome(options=chrome_options)
     driver.implicitly_wait(10)
     driver.get('https://www.dimepkairos.com.br/Dimep/Account/Marcacao')
@@ -22,11 +18,16 @@ def bater_ponto(user,password):
 
 class Scheduler:
     def __init__(self,user,password):
+        self.user=user
+        self.password=password
         for horario in {'09:00', '12:00', '13:00', '18:00'}:
-            schedule.every().monday.at(horario).do(bater_ponto(user,password))
-            schedule.every().tuesday.at(horario).do(bater_ponto(user,password))
-            schedule.every().wednesday.at(horario).do(bater_ponto(user,password))
-            schedule.every().thursday.at(horario).do(bater_ponto(user,password))
-            schedule.every().friday.at(horario).do(bater_ponto(user,password))
+            schedule.every().monday.at(horario).do(self.bater_ponto)
+            schedule.every().tuesday.at(horario).do(self.bater_ponto)
+            schedule.every().wednesday.at(horario).do(self.bater_ponto)
+            schedule.every().thursday.at(horario).do(self.bater_ponto)
+            schedule.every().friday.at(horario).do(self.bater_ponto)
         while True:
             schedule.run_pending()
+    
+    def bater_ponto(self):
+        crawl(self.user,self.password)
